9658c1f54f33cbb9bf8724c093dce762ef168ac4
happyCupcake <36719838+happyCupcake@users.noreply.github.com>
2023-08-05 07:56:17 +0000

    remove toggle menu from home page

---
+++ login-script.js
@@ -0,0 +1,93 @@
+// Login Form
+const loginForm = document.getElementById('login-form');
+const loginMessage = document.getElementById('login-message');
+const userInfo = document.getElementById('user-info');
+const dashboard = document.getElementById('dashboard');
+const logoutButton = document.getElementById('logout-button');
+
+loginForm.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const loginContact = document.getElementById('login-contact').value;
+  const loginPassword = document.getElementById('login-password').value;
+
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+
+    const storedUser = JSON.parse(value);
+
+    if (storedUser) {
+      if (storedUser.contact === loginContact && storedUser.password === loginPassword) {
+        loginMessage.textContent = 'Login successful!';
+        loginForm.reset();
+        showLoggedInUser(key);
+        break;
+      } else {
+        loginMessage.textContent = 'Invalid contact method or password.';
+      }
+    } else {
+      loginMessage.textContent = 'No user found. Please register.';
+    }
+  }
+});
+
+function showLoggedInUser(userKey) {
+  var user = JSON.parse(localStorage.getItem(userKey));
+  if (user) {
+    if (user.polling == '') {
+      gapi.client.setApiKey('AIzaSyB7q8BVVatQbt7btIRwPPbWq_5ZJlp0vq4');
+      var elementId = 'user-info';
+      console.log('showLoggedInUser', user.address);
+      htmlInfo = `
+            <p><strong>Name:</strong> ${user.name}</p>
+            <p><strong>Address:</strong> ${user.address}</p>
+            <p><strong>Contact Method:</strong> ${user.contact}</p>
+            <p></p>
+            <p></p>
+          `;
+      lookup(user.address, function(response) {
+        displayPollingLocation(user, response, elementId, htmlInfo);
+      });
+    } else {
+      userInfo.innerHTML = `
+            <p><strong>Name:</strong> ${user.name}</p>
+            <p><strong>Address:</strong> ${user.address}</p>
+            <p><strong>Contact Method:</strong> ${user.contact}</p>
+            <p><strong>Polling Location:</string> ${user.polling}</p>
+            <p></p>
+            <p></p>`
+    }
+    // find all users with same polling location as me
+    userInfo.innerHTML += `
+          <hr>
+          <p><strong>Additional users found with same polling location:</strong></p>`
+
+    for (var i = 0; i < localStorage.length; i++) {
+      var key = localStorage.key(i);
+      var value = localStorage.getItem(key);
+      const storedUser = JSON.parse(value);
+
+      if (storedUser) {
+        if (storedUser.polling === user.polling && storedUser.name != user.name) {
+          userInfo.innerHTML += `
+                <p><strong>Contact Method:</strong> ${storedUser.contact}</p>`
+        } else {
+          loginMessage.textContent = 'Invalid contact method or password.';
+        }
+      } else {
+        loginMessage.textContent = 'No user found with same polling location.';
+      }
+
+    }
+    userInfo.innerHTML += `
+       <p> <strong>You can start planning your Voting Parties by initiating an Email.</strong></p>`
+  }
+  loginForm.style.display = 'none';
+  dashboard.style.display = 'block';
+}
+
+logoutButton.addEventListener('click', function() {
+  loginForm.style.display = 'block';
+  dashboard.style.display = 'none';
+});
---
+++ registration-script.js
@@ -0,0 +1,36 @@
+// Registration Form
+const registrationForm = document.getElementById('registration-form');
+const registrationMessage = document.getElementById('registration-message');
+
+registrationForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const address = document.getElementById('address').value;
+  const contact = document.getElementById('contact').value;
+  const password = document.getElementById('password').value;
+
+  const user = {
+    name: name,
+    address: address,
+    contact: contact,
+    password: password,
+    polling: '',
+  };
+
+  console.log(user.name + " " + user.contact + " " + user.password)
+  localStorage.setItem(user.contact, JSON.stringify(user));
+
+  registrationMessage.textContent = 'Registration complete!';
+  registrationForm.reset();
+
+  showLoggedInUser(user.contact);
+});
+
+function showLoggedInUser(userKey) {
+  var user = JSON.parse(localStorage.getItem(userKey));
+  if (user) {
+    // Display user info after registration
+    console.log(user);
+  }
+}
